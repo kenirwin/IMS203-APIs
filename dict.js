a55ffae9df0adcbba4ce50fb7a16b5eb08385f7d
Ken Irwin <irwinkr@miamioh.edu>
2021-03-11 16:35:23 -0500

    better handling of audio pronunciation

--- dict.js
+++ dict.js
@@ -27,54 +27,63 @@ $(document).ready(function () {
   }
 
   function Lookup(word, dict) {
+    $('.alert').hide();
     url = urls[dict] + word;
-
-    console.log(url);
     let res = $.get(url, function (data) {
-      let defs = JSON.parse(res.responseText)[0].shortdef;
-      if (dict == 'eng') {
-        lang = 'en';
-        country = 'us';
-        pron = JSON.parse(res.responseText)[0].hwi.prs[0].sound.audio;
-      } else if (dict == 'spa') {
-        lang = 'es';
-        country = 'me';
-        pron = JSON.parse(res.responseText)[0].hwi.prs[0].sound.audio;
+      if (
+        JSON.parse(res.responseText)[0] &&
+        JSON.parse(res.responseText)[0].shortdef
+      ) {
+        // console.log('found', JSON.parse(res.responseText));
+        let defs = JSON.parse(res.responseText)[0].shortdef;
+        if (dict == 'eng') {
+          lang = 'en';
+          country = 'us';
+          pron = JSON.parse(res.responseText)[0].hwi.prs[0].sound.audio;
+        } else if (dict == 'spa') {
+          lang = 'es';
+          country = 'me';
+          pron = JSON.parse(res.responseText)[0].hwi.prs[0].sound.audio;
+        }
+
+        letter = pron.substring(0, 1);
+
+        audioUrl =
+          'https://media.merriam-webster.com/audio/prons/' +
+          lang +
+          '/' +
+          country +
+          '/mp3/' +
+          letter +
+          '/' +
+          pron +
+          '.mp3';
+        audio =
+          '<audio controls><source src="' +
+          audioUrl +
+          '" type="audio/mpeg"></audio>';
+        console.log(defs);
+        console.log(audioUrl);
+        let defHtml = '';
+        defs.forEach((def) => {
+          defHtml += '<li>' + def + '</li>';
+        });
+        $('#' + dict + 'Term')
+          .html(
+            '<h2 class="defTerm">' +
+              audio +
+              word +
+              '</h2><ol>' +
+              defHtml +
+              '</ol>'
+          )
+          .show();
+      } else {
+        console.log('not found in ', dict);
+        $('#' + dict + 'Term')
+          .html('<h2 class="defTerm">' + word + '</h2>' + 'Not found')
+          .show();
       }
-
-      letter = pron.substring(0, 1);
-
-      audioUrl =
-        'https://media.merriam-webster.com/audio/prons/' +
-        lang +
-        '/' +
-        country +
-        '/mp3/' +
-        letter +
-        '/' +
-        pron +
-        '.mp3';
-      audio =
-        '<audio controls><source src="' +
-        audioUrl +
-        '" type="audio/mpeg"></audio>';
-      console.log(defs);
-      console.log(audioUrl);
-      let defHtml = '';
-      defs.forEach((def) => {
-        defHtml += '<li>' + def + '</li>';
-      });
-      $('.alert').hide();
-      $('#' + dict + 'Term')
-        .html(
-          '<h2 class="defTerm">' +
-            word +
-            '</h2><ol>' +
-            defHtml +
-            '</ol>' +
-            audio
-        )
-        .show();
     });
   }
 });
